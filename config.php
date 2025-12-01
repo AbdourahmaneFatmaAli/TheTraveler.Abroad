0228eaae69fd0229bfea906e454a14480056be94
Fatma Ali <123450761+AbdourahmaneFatmaAli@users.noreply.github.com>
2025-11-30 18:11:47 -0800

    Change database username and password
    Updated database connection credentials.

--- config.php
+++ config.php
@@ -1,15 +1,16 @@
 <?php
 $host = 'localhost';
 $dbname = 'webtech_project';
-$username = 'root';
-$password = '';
+$username = 'fatma.abdourahmane';
+$password = 'Fatou019';  
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  
 } catch (PDOException $e) {
     die("Could not connect to the database $dbname: " . $e->getMessage());
 }
 ?>
+
+
